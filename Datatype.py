d0fe5c8758959e6c9b0c2ee2f66db86c16875a36
safiahzain <safiahzain7@gmail.com>
2025-09-16 07:31:25 +0300

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,25 @@
+##Datatype##
+number=1
+
+flnum=5.5
+
+text= "safiah's book"
+
+text1='safiah'
+
+text2="""line one
+line two"""
+
+bool=True
+
+fruit=["orang","cherry","apple"]
+
+things=("car","sky","shoes")
+
+Employee={
+    "name":"Sara",
+    "salary":1000,
+    "department":"IT"
+}
+
+colors={"black","green","Pink"}
